d8e31c0c5801a9e31f0d2efcf418798a7da2f974
Aleksandr Khan <manchig1990@gmail.com>
2024-03-06 20:58:45 +1000

    add Task 3

--- Example001_Methods/Program.cs
+++ Example001_Methods/Program.cs
@@ -94,32 +94,71 @@
 
 // Ясна задача?
 
-string text = "- Я думаю, - сказал князь, улыбаясь, - что, "
-            + "ежели бы вас послали вместо нашего милого Винценгероде,"
-            + "вы бы взяли приступом согласие прусского короля. "
-            + "Вы так красноречивы. Вы дадите мне чаю?";
+// string text = "- Я думаю, - сказал князь, улыбаясь, - что, "
+//             + "ежели бы вас послали вместо нашего милого Винценгероде,"
+//             + "вы бы взяли приступом согласие прусского короля. "
+//             + "Вы так красноречивы. Вы дадите мне чаю?";
 
-// string s = "qwerty"
-//             012
-// string[3] // r
+// // string s = "qwerty"
+// //             012
+// // string[3] // r
 
-string Replace(string text, char oldValue, char newValue)
+// string Replace(string text, char oldValue, char newValue)
+// {
+//     string result = String.Empty;
+//     int length = text.Length;
+//     for (int i = 0; i < length; i++)
+//     {
+//         if (text[i] == oldValue)
+//             result = result + $"{newValue}";
+//         else
+//             result = result + $"{text[i]}";
+//     }
+//     return result;
+// }
+
+// string newText = Replace(text, ' ', '|');
+// System.Console.WriteLine(newText);
+// System.Console.WriteLine();
+// newText = Replace(newText, 'к', 'К');
+// System.Console.WriteLine(newText);
+// System.Console.WriteLine();
+
+
+// Находим в массиве самое минимальное число и меняем его от [0] до [-1]
+
+int[] arr = { 1, 5, 4, 3, 2, 6, 7, 1, 1 };
+
+void PrintArray(int[] array)
 {
-    string result = String.Empty;
-    int length = text.Length;
-    for (int i = 0; i < length; i++)
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
     {
-        if (text[i] == oldValue)
-            result = result + $"{newValue}";
-        else
-            result = result + $"{text[i]}";
+        System.Console.Write($"{array[i]} ");
     }
-    return result;
+    System.Console.WriteLine();
 }
 
-string newText = Replace(text, ' ', '|');
-System.Console.WriteLine(newText);
-System.Console.WriteLine();
-newText = Replace(newText, 'к', 'К');
-System.Console.WriteLine(newText);
-System.Console.WriteLine();
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] < array[minPosition]) minPosition = j;
+        }
+
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+}
+
+PrintArray(arr);
+SelectionSort(arr);
+
+PrintArray(arr);
+
+// Сделать наоборот, от минимального в максимальный
